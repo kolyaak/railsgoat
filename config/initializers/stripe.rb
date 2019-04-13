e709792bc9e26b463adb46e661b8b3ba6ea83015
Niko <nikolay.akatyev@gmail.com>
2019-04-13 10:39:06 +0800

    Update stripe.rb
    Fixing secret key problem with env variables

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,5 +1,5 @@
 Rails.configuration.stripe = {
-  :publishable_key => 'pk_live_3cFdtK53jFCFSLGH43feG4Fr',
-  :secret_key => 'sk_live_6cFFtK53jFCFSLGH43ffG4Fr'
+  :publishable_key => ENV['PUBLISHABLE_KEY'],
+  :secret_key => ENV['SECRET_KEY']
 }
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
